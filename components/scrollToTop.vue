42f681c0d781bff65529c6410971294f1f1b7466
Sakhile Simelane <51701132+sakhike1@users.noreply.github.com>
2024-09-01 23:39:01 +0200

    updated pages

---
+++ components/scrollToTop.vue
@@ -0,0 +1,40 @@
+<template>
+  <div v-if="showScrollButton" ref="scrollTopButton" class="fixed bottom-[400px] right-10 z-[999]">
+    <button @click="scrollToTop" class="hover:scale-125 text-white font-bold py-2 px-4">
+      <img class="h-10 w-10" :src="upArrowSrc" alt="Scroll to top" />  
+    </button>
+  </div>
+</template>
+
+
+
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+import upArrow from '@/assets/up-arrow.png'; // Import the image directly
+
+const showScrollButton = ref(false);
+const upArrowSrc = ref(upArrow); // Use the imported image path
+
+function handleScroll() {
+  showScrollButton.value = window.scrollY > 0;
+}
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener('scroll', handleScroll);
+});
+</script>
+
+
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
     <images-page/>
     
     <SlidePage />
-  
+     <scrollToTop/>
     <Footer />
   </div>
 </template>
